29e83e318ba4477ef501669a5b26e5ce41842b8f
Ipse-lachiquita <87456617+Ipse-lachiquita@users.noreply.github.com>
2022-08-16 15:56:27 +0900

    #2.15 Conditionals part Three(13:49)
    까지 완료.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const player = {
-  name: 'nico',
-  sayHello: function (otherPersonsName) {
-    console.log('Hello! ' + otherPersonsName + ' nice to meet you!')
-  },
+const age = parseInt(prompt('How old are you?'))
+
+console.log(isNaN(age))
+
+if (isNaN(age) || age < 0) {
+  console.log('Please write a number!!')
+} else if (age < 18) {
+  console.log('You are too young.')
+} else if (age >= 18 && age <= 50) {
+  console.log('You can drink')
+} else if (age > 50 && age <= 80) {
+  console.log('You should exercise')
+} else if (age > 80) {
+  console.log('You can do whatever you want.')
+} else if (age === 100) {
+  console.log('Wow you are wise')
+} else if (age > 80) {
+  console.log('You can do whatever you want.')
 }
-
-console.log(player.name)
-player.sayHello('lynn')
-player.sayHello('nico')
-
-function plus(a, b) {
-  console.log(a + b)
-}
-
-plus(5, 5)
